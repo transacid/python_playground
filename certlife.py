3526b59e3029c456876edd74ccf8a7e1178c3fb1
Boris Petersen <transacid@centerim.org>
2018-08-15 22:57:52 +0200

    exception handling

--- certlife.py
+++ certlife.py
@@ -8,18 +8,26 @@ from termcolor import colored
 
 
 def get_smtp_tls_certificate(hostname, port):
-    connection = smtplib.SMTP(hostname, port)
-    connection.starttls()
-    cert = connection.sock.getpeercert(binary_form=True)
-    return cert
+    try:
+        connection = smtplib.SMTP(hostname, port)
+    except OSError as e:
+        print("Cannot reach host {}:\n\t{}".format(hostname, e))
+    else:
+        connection.starttls()
+        cert = connection.sock.getpeercert(binary_form=True)
+        return cert
 
 
 def get_ssl_certificate(hostname, port):
     context = ssl.create_default_context()
-    with socket.create_connection((hostname, port)) as sock:
-        with context.wrap_socket(sock, server_hostname=hostname) as ssock:
-            cert = ssock.getpeercert(binary_form=True)
-    return cert
+    try:
+        with socket.create_connection((hostname, port)) as sock:
+            with context.wrap_socket(sock, server_hostname=hostname) as ssock:
+                cert = ssock.getpeercert(binary_form=True)
+    except socket.gaierror as e:
+        print("Cannot reach host {}:\n\t{}".format(hostname, e))
+    else:
+        return cert
 
 
 def get_xmpp_tls_certificate(hostname, port):
@@ -27,28 +35,35 @@ def get_xmpp_tls_certificate(hostname, port):
                        " xmlns:tls='http://www.ietf.org/rfc/rfc2595.txt' to='{}'>"
     xmpp_starttls = "<starttls xmlns='urn:ietf:params:xml:ns:xmpp-tls'/>"
 
-    sock = socket.create_connection((hostname, port))
-    sock.send(xmpp_open_stream.format(hostname).encode())
-    sock.recv(2048)
-    sock.send(xmpp_starttls.encode())
-    sock.recv(2048)
-    with ssl.wrap_socket(sock) as ssock:
-        cert = ssock.getpeercert(binary_form=True)
-    return cert
+    try:
+        sock = socket.create_connection((hostname, port))
+    except socket.gaierror as e:
+        print("Cannot reach host {}:\n\t{}".format(hostname, e))
+    else:
+        sock.send(xmpp_open_stream.format(hostname).encode())
+        sock.recv(2048)
+        sock.send(xmpp_starttls.encode())
+        sock.recv(2048)
+        with ssl.wrap_socket(sock) as ssock:
+            cert = ssock.getpeercert(binary_form=True)
+        return cert
 
 
 def openssl(cert):
-    x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_ASN1, cert)
-    raw_date = x509.get_notAfter().decode("ascii")
-    date = datetime.datetime.strptime(raw_date, "%Y%m%d%H%M%SZ")
-    formated_date = datetime.datetime.strftime(date, "%d. %B %Y, %H:%M")
-    return formated_date
+    if cert is not None:
+        x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_ASN1, cert)
+        raw_date = x509.get_notAfter().decode("ascii")
+        date = datetime.datetime.strptime(raw_date, "%Y%m%d%H%M%SZ")
+        formated_date = datetime.datetime.strftime(date, "%d. %B %Y, %H:%M")
+        return formated_date
 
 
 def output(func, hostname, port):
-    print("The Certificate for {} on port {} expires on\n\t{}.\n".format(colored(hostname, "green"),
-                                                                         colored(port, "green"),
-                                                                         colored(openssl(func(hostname, port)), "red")))
+    call_function = func(hostname, port)
+    if call_function is not None:
+        print("The Certificate for {} on port {} expires on\n\t{}.\n".format(colored(hostname, "green"),
+                                                                             colored(port, "green"),
+                                                                             colored(openssl(call_function), "red")))
 
 
 output(get_ssl_certificate, "transacid.de", 443)
